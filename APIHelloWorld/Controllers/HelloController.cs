5d1af08e5ad191ec9956ac5ca1a9805e29aaeac6
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-04-26 11:26:10 +0530

    first commit

---
+++ APIHelloWorld/Controllers/HelloController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIHelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Get executed");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Post Executed");
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok("Put Executed");
+        }
+
+        [HttpPatch]
+        public IActionResult Patch()
+        {
+            return Ok("Patch Executed");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok("Delete Executed");
+        }
+    }
+}
